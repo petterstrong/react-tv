bae1b9efe6dc8494d8e15848f505081c0ee4d9a2
Raphael Amorim <rapha850@gmail.com>
2017-11-09 02:10:26 -0200

    fixup! renderer: remove escapeTextContentForBrowser

--- src/renderer/__tests__/ReactTVFiberEntry-test.js
+++ src/renderer/__tests__/ReactTVFiberEntry-test.js
@@ -6,17 +6,13 @@ const render = ReactTVRenderer.render;
 describe('render', () => {
   it('should transform props into attribute', () => {
     const root = document.createElement('div');
-    const reactElement = React.createElement(
-      'div',
-      {attr: 'custom'},
-      'cowboy bebop'
-    );
+    const ReactElement = <div attr={'custom'}>cowboy bebop</div>;
 
     const expectedElement = document.createElement('div');
     expectedElement.setAttribute('attr', 'custom');
     expectedElement.textContent = 'cowboy bebop';
 
-    expect(render(reactElement, root)).toEqual(expectedElement);
+    expect(render(ReactElement, root)).toEqual(expectedElement);
   });
 
   it('should transform onClick into onclick attribute', () => {
@@ -25,23 +21,22 @@ describe('render', () => {
     }
 
     const textNode = 'uzumaki naruto!';
-
     const root = document.createElement('div');
-    const reactElement = React.createElement('div', {onClick: fn}, textNode);
 
     const expectedElement = document.createElement('div');
     expectedElement.setAttribute('onclick', fn);
     expectedElement.textContent = textNode;
 
-    expect(render(reactElement, root)).toEqual(expectedElement);
+    expect(render(<div onClick={fn}>{textNode}</div>, root)).toEqual(expectedElement);
   });
 
   it('should render with the default toJSON behavior', () => {
-    const Base = () => React.createElement('div');
-    const Page1 = () => React.createElement('div');
-    const Page2 = () => React.createElement('div');
+    const myIncremental = 'it\'s base';
+    const Base = () => <div id={'base'}></div>;
+    const Page1 = () => <div class={'page-1'}></div>;
+    const Page2 = () => <div class={'page-2'}></div>;
 
-    // <Route path={path} component={component}>{children}</Route>
+    // helper for <Route path={path} component={component}>{children}</Route>
     const Route = (path, component, children) =>
       React.createElement(
         'Route',
